5ae5587668af99071287611b459314f77f580edf
Shiwon Park <psw041115@gmail.com>
2024-02-06 02:26:25 +0900

    update controller

--- src/version/version.controller.ts
+++ src/version/version.controller.ts
@@ -1,5 +1,7 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
@@ -14,6 +16,8 @@ import { AccessTokenGuard } from 'src/user/guard/accessToken.guard';
 import { GetUser } from 'src/user/decorator/getUser.decorator';
 import { UserInfo } from 'src/user/types/userInfo.type';
 import { ApiTags } from '@nestjs/swagger';
+import { ContainerDto } from './dto/req/container.dto';
+import { SnippetDto } from './dto/req/snippet.dto';
 
 @ApiTags('version')
 @Controller('project/:projectId/version')
@@ -40,12 +44,48 @@ export class VersionController {
   }
 
   @Post(':versionId/container')
-  async createContainer(): Promise<void> {
+  async createContainer(
+    @Param('projectId', ParseIntPipe) projectId: number,
+    @Param('versionId', ParseIntPipe) versionId: number,
+    @Body() containerDto: ContainerDto,
+    @GetUser() user: UserInfo,
+  ): Promise<void> {
+    return;
+  }
+
+  @Post(':versionId/snippet')
+  async createSnippet(
+    @Param('projectId', ParseIntPipe) projectId: number,
+    @Param('versionId', ParseIntPipe) versionId: number,
+    @Body() snippetDto: SnippetDto,
+    @GetUser() user: UserInfo,
+  ): Promise<void> {
     return;
   }
 
   @Patch(':versionId/sinppet/:snippetId')
   async updateSnippet(
+    @Param('projectId', ParseIntPipe) projectId: number,
+    @Param('versionId', ParseIntPipe) versionId: number,
+    @Param('snippetId', ParseIntPipe) snippetId: number,
+    @Body('content') content: string,
+    @GetUser() user: UserInfo,
+  ): Promise<void> {
+    return;
+  }
+
+  @Delete(':versionId/container/:containerId')
+  async deleteContainer(
+    @Param('projectId', ParseIntPipe) projectId: number,
+    @Param('versionId', ParseIntPipe) versionId: number,
+    @Param('containerId', ParseIntPipe) containerId: number,
+    @GetUser() user: UserInfo,
+  ): Promise<void> {
+    return;
+  }
+
+  @Delete(':versionId/snippet/:snippetId')
+  async deleteSnippet(
     @Param('projectId', ParseIntPipe) projectId: number,
     @Param('versionId', ParseIntPipe) versionId: number,
     @Param('snippetId', ParseIntPipe) snippetId: number,
