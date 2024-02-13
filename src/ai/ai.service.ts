e52224432ea9ac2d57fd0ca6ceacc41fa385306c
Shiwon Park <psw041115@gmail.com>
2024-02-13 19:03:50 +0900

    draft connect ai container

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -1,6 +1,10 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AxiosError } from 'axios';
+import { firstValueFrom } from 'rxjs';
+import { RegenerateQueryDto } from './dto/req/regenerateQuery.dto';
+import { GenerateQueryDto } from './dto/req/generateQuery.dto';
 
 @Injectable()
 export class AiService {
@@ -9,23 +13,103 @@ export class AiService {
     private readonly configSerivce: ConfigService,
   ) {}
 
-  async generateContainer() {
-    return;
+  async generateContainer(title: string): Promise<Object> {
+    const result = await firstValueFrom(
+      this.httpService.get<Object>(
+        this.configSerivce.getOrThrow<string>('AI_SERVER_URL') +
+          '/generate-container/' +
+          title,
+      ),
+    ).catch((error) => {
+      if (error instanceof AxiosError) {
+        throw new InternalServerErrorException(
+          'AI server Error: ' + error.message,
+        );
+      }
+      throw new InternalServerErrorException();
+    });
+    return result.data;
   }
 
-  async generateQuery() {
-    return;
+  async generateQuery(generateQueryDto: GenerateQueryDto): Promise<Object> {
+    const result = await firstValueFrom(
+      this.httpService.post<Object>(
+        this.configSerivce.getOrThrow<string>('AI_SERVER_URL') +
+          '/query-generator',
+        generateQueryDto,
+      ),
+    ).catch((error) => {
+      if (error instanceof AxiosError) {
+        throw new InternalServerErrorException(
+          'AI server Error: ' + error.message,
+        );
+      }
+      throw new InternalServerErrorException();
+    });
+    return result.data;
   }
 
-  async useGoogleSearch() {
-    return;
+  async regenerateQuery(
+    regenerateQueryDto: RegenerateQueryDto,
+  ): Promise<Object> {
+    const result = await firstValueFrom(
+      this.httpService.post<Object>(
+        this.configSerivce.getOrThrow<string>('AI_SERVER_URL') +
+          '/query-regenerator',
+        regenerateQueryDto,
+      ),
+    ).catch((error) => {
+      if (error instanceof AxiosError) {
+        throw new InternalServerErrorException(
+          'AI server Error: ' + error.message,
+        );
+      }
+      throw new InternalServerErrorException();
+    });
+    return result.data;
   }
 
-  async useVectorSearch() {
-    return;
+  async useGoogleSearch(query: string, n: number): Promise<Object> {
+    const result = await firstValueFrom(
+      this.httpService.get<Object>(
+        this.configSerivce.getOrThrow<string>('AI_SERVER_URL') +
+          '/google-search/' +
+          query +
+          '/' +
+          n,
+      ),
+    ).catch((error) => {
+      if (error instanceof AxiosError) {
+        throw new InternalServerErrorException(
+          'AI server Error: ' + error.message,
+        );
+      }
+      throw new InternalServerErrorException();
+    });
+    return result.data;
   }
 
-  async extractorSnippet() {
+  async useVectorSearch(query: string, n: number): Promise<Object> {
+    const result = await firstValueFrom(
+      this.httpService.get<Object>(
+        this.configSerivce.getOrThrow<string>('AI_SERVER_URL') +
+          '/vector-search/' +
+          query +
+          '/' +
+          n,
+      ),
+    ).catch((error) => {
+      if (error instanceof AxiosError) {
+        throw new InternalServerErrorException(
+          'AI server Error: ' + error.message,
+        );
+      }
+      throw new InternalServerErrorException();
+    });
+    return result.data;
+  }
+
+  async extractSnippet() {
     return;
   }
 }
---
+++ src/ai/dto/req/regenerateQuery.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsInt, IsNumber, IsString } from 'class-validator';
+
+export class RegenerateQueryDto {
+  @ApiProperty()
+  @IsArray()
+  @IsString({ each: true })
+  allContents: string[];
+
+  @ApiProperty()
+  @IsString()
+  focusedContainer: string;
+
+  @ApiProperty()
+  @IsString()
+  guildingVector: string;
+
+  @ApiProperty()
+  @IsString()
+  previousQuery: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsArray()
+  shownSnippets: string[];
+
+  @ApiProperty()
+  @IsString()
+  prefferedSnippet: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsInt()
+  n: string;
+}
